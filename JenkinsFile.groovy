7fdac78c8f282455ba8ec20b7b8a293bdbe01862
piyachet <37733326+piyachet@users.noreply.github.com>
2024-02-26 23:17:07 +0700

    Update JenkinsFile.groovy

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -22,7 +22,7 @@ pipeline {
         stage('Build') {
             steps {
                 script {
-                    docker.build("${DOCKER_IMAGE_NAME}:${BUILD_NUMBER}", "-f Dockerfile .")
+                    docker.build("${DOCKER_IMAGE_NAME}:latest", "-f Dockerfile .")
                 }
             }
         }
