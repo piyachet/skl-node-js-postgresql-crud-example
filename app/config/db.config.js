bc75f044d8bd5b92e4fe9fd3232f6db83ab743b2
Piyachet Rungsrisuwakul <piyachet.run@ttbbank.com>
2024-02-27 15:09:21 +0700

    ADD Deployment

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: "skl-crud-db.crigquc2s6l4.ap-southeast-1.rds.amazonaws.com",
   USER: "postgres",
   PASSWORD: "password",
-  DB: "testdb",
+  DB: "postgres",
   dialect: "postgres",
   pool: {
     max: 5,
