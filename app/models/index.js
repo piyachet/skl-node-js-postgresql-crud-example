0261c1d7bafcd8d847ab01cc86a383ec1943b041
Piyachet Rungsrisuwakul <piyachet.run@ttbbank.com>
2024-02-27 19:37:41 +0700

    ADD Deployment

--- app/models/index.js
+++ app/models/index.js
@@ -5,12 +5,12 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+  // pool: {
+  //   max: dbConfig.pool.max,
+  //   min: dbConfig.pool.min,
+  //   acquire: dbConfig.pool.acquire,
+  //   idle: dbConfig.pool.idle
+  // }
 });
 
 const db = {};
