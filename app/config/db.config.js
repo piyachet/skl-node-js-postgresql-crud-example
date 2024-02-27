d2dfb891865a2d8bae63f466c573dc55dd5c56f9
Piyachet Rungsrisuwakul <piyachet.run@ttbbank.com>
2024-02-27 18:11:50 +0700

    ADD Deployment

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-  HOST: "database-1.crigquc2s6l4.ap-southeast-1.rds.amazonaws.com",
+  HOST: "skl-crud-nodejs.crigquc2s6l4.ap-southeast-1.rds.amazonaws.com",
   USER: "postgres",
   PASSWORD: "password",
   DB: "testdb",
   dialect: "postgres",
-  dialectOptions: {
-  ssl: {
-    require: true, // This will help you. But you will see nwe error
-     rejectUnauthorized: false // This line will fix new error
-  }
-},
   pool: {
     max: 5,
     min: 0,
